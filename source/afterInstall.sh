013368108df20b4ba76e58f51dd156b4ff6cf36b
Khaled Gamal <khaledgamal@localhost.localdomain>
2020-06-22 00:51:55 +0200

    adding file to first after running composer

---
+++ source/afterInstall.sh
@@ -0,0 +1,8 @@
+# Fix Storage Permessions
+chmod 755 -R storage/logs/
+chmod 777 -R storage/framework/sessions/
+chmod 777 -R storage/framework/views/
+chmod 777 -R bootstrap/cache/
+
+# Optmize Application
+php artisan optimize:clear
