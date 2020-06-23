0815276971cfe3c423cbfc1ef09456120d4672b6
Khaled Gamal <khaledgamal@localhost.localdomain>
2020-06-23 08:08:22 +0200

    adding advertisers entries as config array

---
+++ source/config/advertisers.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+    |--------------------------------------------------------------------------
+    | Advertiser Names
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing advertiser names so in the future we can add new advertisers
+    |
+    */
+return [
+    'airbnb',
+    'booking',
+];
