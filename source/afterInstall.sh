60b6100931983dd712118d67c00c7fb46b244d1c
Khaled Gamal <khaledgamal@localhost.localdomain>
2020-06-23 08:12:11 +0200

    adding route to hotels api

--- source/afterInstall.sh
+++ source/afterInstall.sh
@@ -2,6 +2,7 @@
 chmod 755 -R storage/logs/
 chmod 777 -R storage/framework/sessions/
 chmod 777 -R storage/framework/views/
+chmod 777 -R storage/framework/cache/
 chmod 777 -R bootstrap/cache/
 
 # Optmize Application
